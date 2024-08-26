f5145dc69953e1a16d26459ad85cf2c214b2de15
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-08-26 20:49:38 +0530

    Add code for hashing and vector operations

---
+++ learnhashing.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+//precompute
+int hash[13]={0};
+for(int i=0;i<n;i++){
+    hash[arr[i]]+=1;
+}
+
+    int q;
+    cin>>q;
+    while(q--){
+        int x;
+        cin>>x;
+
+        //fetch 
+cout<<hash[x]<<endl;
+      
+    }
+    return 0;
+
+ }
