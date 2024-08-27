cce0f136bf92d50f51fd721d6f437376db6316a5
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-08-28 00:17:49 +0530

    changed something about the best case complexity.

--- sorting.cpp
+++ sorting.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void selection_sort(int arr[],int n){
+void selection_sort(int arr[],int n){    //selection sort
     for(int i=0;i<=n-2;i++){
          int min=i;
         for(int j=i;j<=n-1;j++){
@@ -19,16 +19,26 @@ void selection_sort(int arr[],int n){
 
 }
 void bubbleSort(int arr[], int n) {
-        // Your code here
-        for (int i = n - 1; i >= 0; i--) {
+
+        // Your code here     //bubble sort
+        for (int i = n - 1; i >= 0; i--) 
+        
+        {
+            int didSwap=0;
         for (int j = 0; j <= i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                 didSwap=1;
             }
         }
+        if(didSwap == 0){
+            break;
         }
+        cout<<"runs\n"<<endl;
+        }
+        
     }
 
 
@@ -40,12 +50,12 @@ int arr[n];
 for(int i=0;i<=n;i++){
     cin>>arr[i];
 }
-selection_sort(arr,n);
-    for(int i=0;i<=n;i++){
-    cout<<arr[i]<<" ";
-}
+// selection_sort(arr,n);
+//     for(int i=0;i<=n;i++){
+//     cout<<arr[i]<<" ";
+// }
 
-cout<<endl;
+// cout<<endl;
 
 bubbleSort(arr,n);
 for(int i=0;i<=n;i++){
