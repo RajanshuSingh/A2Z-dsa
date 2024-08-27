c562e7f9926c95c6cb7b57b57049f9613a4bee8b
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-08-27 23:55:51 +0530

    selection and bubble sort

---
+++ insertion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertion_sort(int arr[],int n){
+    
+
+}
+
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<=n;i++){
+    cin>>arr[i];
+}
+insertion_sort(arr,n);
+    for(int i=0;i<=n;i++){
+    cout<<arr[i]<<" ";
+}
+
+return 0;
+ }
