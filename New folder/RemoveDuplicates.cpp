ebea2e8cb8e719e136d8479453fa65276d304c9a
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-08-30 00:08:55 +0530

    feat: Add code to remove duplicates from an array

---
+++ New folder/RemoveDuplicates.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+int main() {
+  int arr[] = {0,0,0,1,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = removeDuplicates(arr, n);
+  cout << "The array after removing duplicate elements is " << endl;
+  for (int i = 0; i < k; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
