85774b5df89863ce8246e06ac36550d6cc449e65
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-08-30 00:18:05 +0530

    feat: Refactor code to remove duplicates from an array

--- New folder/RemoveDuplicates.cpp
+++ New folder/RemoveDuplicates.cpp
@@ -6,12 +6,13 @@ int removeDuplicates(int arr[], int n)
 {
   int i = 0;
   for (int j = 1; j < n; j++) {
-    if (arr[i] != arr[j]) {
+    if (arr[i] != arr[j]) {   //if the element is not equal to the next element then increment i and assign the value of j to i
+      
+      arr[i+1] = arr[j];   
       i++;
-      arr[i] = arr[j];
     }
   }
-  return i + 1;
+  return i + 1;  //returns size of the array after removing duplicates
 }
 int main() {
   int arr[] = {0,0,0,1,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5};
