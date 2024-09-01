1d7571ce1ac8c00f8e16517fc34288bbc2c5e848
Singh_Rajanshu <112928611+RajanshuSingh@users.noreply.github.com>
2024-09-01 23:37:03 +0530

    it tells about the no. of consecutive ones present in the array

--- Arrays/MaxCons1.cpp
+++ Arrays/MaxCons1.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void maxCons1(vector<int> &arr,int n){
+void maxCons1(vector<int> &arr,int n){   //optimal approach
     int count = 0;
     int maxCount = 0;
     for(int i=0;i<n;i++){
         if(arr[i]==1){
             count++;
-            maxCount = max(maxCount,count);
+            maxCount = max(maxCount,count);   
         }
         else{
             count = 0;
