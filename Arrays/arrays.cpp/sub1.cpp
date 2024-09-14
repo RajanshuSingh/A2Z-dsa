08f9c98f1fafe72cf657188aa7ea5125fc9bf524
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-09-14 22:14:21 +0530

    feat: Add code to find all subarrays with a given sum
    This commit adds a new function, `findAllSubarraysWithGivenSum`, to the code. This function takes a vector of integers and a target sum as input and returns the number of subarrays in the vector that have the given sum. The function uses nested loops to iterate through all possible subarrays and calculates their sum. If the sum matches the target sum, the count is incremented. The main function is also updated to demonstrate the usage of the new function.

---
+++ Arrays/arrays.cpp/sub1.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector <int> arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+
+
