420ad4d98240780e4112838e441a5bb55cb053e1
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-09-18 22:51:54 +0530

    sort 0  1 2

---
+++ Arrays/arrays.cpp/sort12.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    void sortColors(vector<int>& nums) {
+
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1; // high initialized to n-1
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else if (nums[mid] == 2) { // correct condition
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+
