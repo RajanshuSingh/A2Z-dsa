5f8e01e95cdf06db84e5431f9ae52d457ab1338d
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-22 20:38:51 +0530

    print srt of number

---
+++ Binary Search/BSonAnswers/sqrt.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int floorSqrt(int n){
+     
+     int m = sqrt (n);
+        return m;
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+int main() {
+    int n= 16;
+    cout<<floorSqrt(n)<<endl;
+    return 0;
+
+}
