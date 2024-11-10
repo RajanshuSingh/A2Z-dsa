7f3911d704e1db728e89fb0437e17544efaff082
Singh_Rajanshu <112928611+RajanshuSingh@users.noreply.github.com>
2024-11-10 23:25:47 +0530

    climb stairs

---
+++ h.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int a = 1, b = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+};
