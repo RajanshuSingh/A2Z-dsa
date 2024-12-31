66fd0ecadeb8fe0d2ec80190d8fe061964017202
Singh_Rajanshu <112928611+RajanshuSingh@users.noreply.github.com>
2024-12-31 17:12:47 +0530

    oopm

---
+++ oopm/07_AccessModifier.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Human{
+private:
+int a;
+
+void fun(){
+     a=10;
+     b=20;
+    c=30;
+}
+
+protected:
+int b;
+
+
+
+public:
+int c;
+
+
+};
+
+
+int main(){
+
+Human Rajanshu;
+// Rajanshu.a=10;
+// Rajanshu.b=20;
+// Rajanshu.c=30;
+
+// Rajanshu.fun();
+
+// cout<<Rajanshu.<<endl;
+}
